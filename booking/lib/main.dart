660178a4815270032237247f06819bf2b815f5ce
aanaaguilar <55719354+aanaaguilar@users.noreply.github.com>
2023-10-21 18:58:29 -0600

    Update main.dart

--- booking/lib/main.dart
+++ booking/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(useMaterial3: true),
-      home: home(),
+      home: HomePage(),
     );
   }
 }
